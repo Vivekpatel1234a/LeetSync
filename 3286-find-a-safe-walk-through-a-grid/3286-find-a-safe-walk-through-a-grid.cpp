e5e294ba1ac758dd9d0923046c1191b91c53e38c
Vivek_404_X <patelvpv369@gmail.com>
2026-07-02 17:19:24 +0530

    Time: 108 ms (18.8%), Space: 52 MB (17.72%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -4,18 +4,12 @@ public:
         int m=grid.size();
         int n=grid[0].size();
        vector<vector<int>>visited(m,vector<int>(n,0));
-        // for(int i=0; i<m; i++){
-        //     for(int j=0; j<n; j++){
-        //         visited[i][j]=grid[i][j];
-        //     }
-        // }
         priority_queue<vector<int>>pq;
         vector<vector<int>>mat{{1,0},{0,1},{-1,0},{0,-1}};
         int h,i,j,i1,j1;
         if(grid[0][0]==1)health--;
         visited[0][0]=health;
         pq.push({health,0,0});
-        //visited[0][0]=1;
         while(!pq.empty()){
             //int sz=pq.size();
             //while(sz--){
@@ -24,13 +18,11 @@ public:
                 h=it[0];
                 i=it[1];
                 j=it[2];
-                cout<<h<<" "<<i<<" "<<j<<"    ";
                 if(i==m-1 && j==n-1)return true;
                 for(auto ele:mat){
                    i1=ele[0];
                    j1=ele[1];
                    if(i+i1<m && i+i1>=0 && j+j1<n && j+j1>=0){
-                    //visited[i+i1][j+j1]=1;
                     int nh=h;
                     if(grid[i+i1][j+j1]==1) nh--;
                     if(nh>visited[i+i1][j+j1]){
@@ -38,8 +30,6 @@ public:
                         pq.push({nh,i+i1,j+j1});
                     }
                    }
-                //}  
-             
             }
         }
         return false;
