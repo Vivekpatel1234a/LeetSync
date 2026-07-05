4ce389daf0f7eb09b4da1a3fcb78471654f7d98f
Vivek_404_X <patelvpv369@gmail.com>
2026-07-05 16:45:23 +0530

    Time: 0 ms (100%), Space: 30.7 MB (98.41%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.cpp
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    
+    int getrange(int n){
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        while(n>0){
+            maxi=max(maxi,n%10);
+            mini=min(mini,n%10);
+            n/=10;
+        }
+         return maxi-mini;
+    }
+
+    int maxDigitRange(vector<int>& nums) {
+        int topMax=0;
+        for(auto ele:nums){
+            topMax=max(topMax,getrange(ele));
+        }
+        int ans=0;
+        for(auto ele: nums){
+            if(getrange(ele)==topMax)ans+=ele;
+        }
+        return ans;
+    }
+};
+/* SC=O(n) TC=O(n.d);
+
+class Solution {
+public:
     int maxDigitRange(vector<int>& nums) {
         vector<vector<int>>store(10);
         int topMax=0;
@@ -21,4 +49,4 @@ public:
         for(auto ele:store[topMax])result+=ele;
         return result;
     }
-};
+};*/
