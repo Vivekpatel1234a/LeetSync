e77f126dc2dfeda45e357060ae284b74dd663f49
Vivek_404_X <patelvpv369@gmail.com>
2026-07-08 15:00:04 +0530

    Time: 1 ms (64.15%), Space: 14 MB (76.15%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
 
-    void solve(vector<vector<int>>&ans, int i, vector<int>&candidates, int target,int sum,vector<int>temp){
+    void solve(vector<vector<int>>&ans, int i, vector<int>&candidates, int target,int sum,vector<int>&temp){
         int n=candidates.size();
         if(sum>target){
             return;
         }
         if(sum==target){
             ans.push_back(temp);
+            return;
         }
         for(int k=i; k<n; k++){
             temp.push_back(candidates[k]);
             solve(ans,k,candidates,target,sum+candidates[k],temp);
-            temp.pop_back();
+            temp.pop_back();  
         }
         return;
     }
@@ -25,3 +26,33 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+
+    void solve(vector<vector<int>>&ans, int i, vector<int>&candidates, int target,int sum,vector<int>&temp){
+        int n=candidates.size();
+        if(sum==target){
+            ans.push_back(temp);
+            return;
+        }
+        for(int k=i; k<n; k++){
+            if(sum+candidates[k]<=target){//jaane se pehe hi check krlo if valid then go otherwise no
+            temp.push_back(candidates[k]);
+            solve(ans,k,candidates,target,sum+candidates[k],temp);
+            temp.pop_back();
+            }
+        }
+        return;
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        int sum=0;
+        vector<int>temp;
+        solve(ans,0,candidates,target,sum,temp);
+        return ans;
+    }
+};
+*/
