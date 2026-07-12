33896f40e45d6222edba5f224a689ae8d1f9f8c9
Vivek_404_X <patelvpv369@gmail.com>
2026-07-12 11:54:03 +0530

    Time: 35 ms (88.64%), Space: 43.8 MB (71.43%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -2,6 +2,31 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n=arr.size();
+        vector<int>ans=arr;
+        sort(ans.begin(),ans.end());
+       unordered_map<int,int>mp;
+       int order=1;
+       for(int i=0; i<n; i++){
+        if(!mp.count(ans[i])){
+            mp[ans[i]]=order;
+            order++;
+        }
+       }
+       
+       for(int i=0; i<n; i++){
+        arr[i]=mp[arr[i]];
+       }
+        return arr;
+    }
+};
+
+
+
+//not optimal but good 
+/*class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n=arr.size();
        map<int,vector<int>>mp;
        for(int i=0; i<n; i++)mp[arr[i]].push_back(i);
        int order=0;
@@ -11,4 +36,4 @@ public:
        }
         return arr;
     }
-};
+};*/
