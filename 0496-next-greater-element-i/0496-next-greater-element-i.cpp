f348189d900dcfa4d4ba322692418d75f0dd0eaf
Vivek_404_X <patelvpv369@gmail.com>
2026-07-17 14:04:29 +0530

    Time: 0 ms (100%), Space: 13 MB (15.7%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,10 +4,6 @@ public:
         unordered_map<int,int>mp;
         int n2=nums2.size();
         int n=nums1.size();
-        for(int i=0; i<n2; i++){
-            mp[nums2[i]]=i;
-        }
-        vector<int>ng(n2);
         stack<int>st;
         int j=n2-1;
         while(j>=0){
@@ -15,18 +11,19 @@ public:
                 st.pop();
             }
             if(st.empty()){
-                ng[j]=-1;
+                mp[nums2[j]]=-1;
                 st.push(nums2[j]);
+                
             }
             else{
-                ng[j]=st.top();
+                mp[nums2[j]]=st.top();
                 st.push(nums2[j]);
             }
             j--;
         }
         vector<int>ans(n);
         for(int i=0; i<n; i++){
-            ans[i]=ng[mp[nums1[i]]];
+            ans[i]=mp[nums1[i]];
         }
 
         return ans;
