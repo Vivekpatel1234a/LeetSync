b3c9989b6085e7e58321bc8b2935e14b5e152f60
Vivek_404_X <patelvpv369@gmail.com>
2026-07-17 14:07:37 +0530

    Time: 2 ms (47.02%), Space: 12.7 MB (62.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -7,13 +7,12 @@ public:
         stack<int>st;
         int j=n2-1;
         while(j>=0){
-            while(!st.empty() && st.top()<nums2[j]){
+            while(!st.empty() && st.top()<=nums2[j]){
                 st.pop();
             }
             if(st.empty()){
                 mp[nums2[j]]=-1;
                 st.push(nums2[j]);
-                
             }
             else{
                 mp[nums2[j]]=st.top();
@@ -25,7 +24,6 @@ public:
         for(int i=0; i<n; i++){
             ans[i]=mp[nums1[i]];
         }
-
         return ans;
     }
 };
