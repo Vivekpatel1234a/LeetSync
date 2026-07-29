28dfcf2cffdd029ac0c260687b09724c2f3ae390
Vivek_404_X <patelvpv369@gmail.com>
2026-07-29 08:03:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.31%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dp[46];
+    int solve(int n){
+        if(dp[n]!=-1)return dp[n];
+        if(n==1)return 1;
+        if(n==2)return 2;
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp));
+        int ans=solve(n);
+        return ans;
+    }
+};
