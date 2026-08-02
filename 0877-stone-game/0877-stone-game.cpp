6bac5729536a1a3f27e9b35bda2f338533687b06
Vivek_404_X <patelvpv369@gmail.com>
2026-08-02 17:11:59 +0530

    Time: 0 ms (100%), Space: 10.6 MB (39.52%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int i=0;
+        int n=piles.size();
+        int j=n-1;
+        int sumA=0;
+        int sumB=0;
+        while(i<j){
+            if(piles[i]<piles[j]){
+                sumA+=piles[j];
+                sumB+=piles[i];
+            }
+            else{
+                sumA+=piles[i];
+                sumB+=piles[j];
+            }
+            i++;
+            j--;
+        }
+        return sumA>=sumB;
     }
 };
