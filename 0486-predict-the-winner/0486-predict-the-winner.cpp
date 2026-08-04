e0d36b8ab88428ccee9376950f128e20e7ce3031
Vivek_404_X <patelvpv369@gmail.com>
2026-08-04 22:41:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (68.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-
+    int dp[21][21];
     int solve(int i, int j, vector<int>&nums){
+        
         if(i>j)return 0;
-        int ans=max(nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums)),nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums)));
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=dp[i][j]=max(nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums)),nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums)));
         
         return ans;
 
     }
 
     bool predictTheWinner(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
       int n=nums.size();
       int sum=0;
       for(auto ele:nums)sum+=ele;
