139f6853d587d156e0a5e83d130df361881f7419
Vivek_404_X <patelvpv369@gmail.com>
2026-08-06 23:13:32 +0530

    Time: 2 ms (62.7%), Space: 118 MB (82.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,10 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //do it using middle half 
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-       stack<int>st;
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast!=NULL && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+       }
+       //coz slow is at middle 
+       ListNode* head1=slow;
+       ListNode* temp=head1;
+        ListNode* prev=NULL;
+        while(temp!=NULL){
+            temp=head1->next;
+            head1->next=prev;
+            prev=head1;
+            head1=temp;
+        }
+        while(prev!=NULL && head!=NULL){
+            if(prev->val!=head->val)return 0;
+            prev=prev->next;
+            head=head->next;
+        }
+
+
+       /*stack<int>st;
        ListNode* temp=head;
         while(temp!=NULL){
             st.push(temp->val);
@@ -21,7 +45,7 @@ public:
             if(head->val!=st.top())return 0;
             head=head->next;
             st.pop();
-        }
+        }*/
 
         return 1;
     }
