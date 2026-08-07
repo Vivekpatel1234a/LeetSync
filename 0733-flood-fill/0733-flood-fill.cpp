ac21865005f7a7cac43b319b0328847a8786818d
Vivek_404_X <patelvpv369@gmail.com>
2026-08-07 23:51:07 +0530

    Time: 0 ms (100%), Space: 18 MB (75.64%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    
-  void  solve(int r,int c, int color,vector<vector<int>>& image,vector<vector<int>>& visited,int m,int n,int init){
-        if(visited[r][c])return;
-        visited[r][c]=1;
+    int old=0;
+  void  solve(int r,int c, int color,vector<vector<int>>& image,int m,int n){
+        
+        if(r>=m || c>=n || r<0 || c<0)return;
+        if(image[r][c]!=old)return;
         image[r][c]=color;
-        if(r+1<m && image[r+1][c]==init){
-            solve(r+1,c,color,image,visited,m,n,init);
-        }
-        if(c+1<n && image[r][c+1]==init){
-            solve(r,c+1,color,image,visited,m,n,init);
-        }
-        if(r-1>=0 && image[r-1][c]==init){
-            solve(r-1,c,color,image,visited,m,n,init);
-        }
-        if(c-1>=0 && image[r][c-1]==init){
-            solve(r,c-1,color,image,visited,m,n,init);
-        }
+            solve(r+1,c,color,image,m,n);
+            solve(r,c+1,color,image,m,n);
+            solve(r-1,c,color,image,m,n);
+            solve(r,c-1,color,image,m,n);
         //up
         //down
         //left
@@ -27,9 +20,9 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
      int m=image.size();
      int n=image[0].size();
-     vector<vector<int>>vis(m,vector<int>(n,0));
-     int init=image[sr][sc];
-     solve(sr,sc,color,image,vis,m,n,init);
+     old=image[sr][sc];
+     if(old==color)return image;
+     solve(sr,sc,color,image,m,n);
      return image;  
     }
 };
