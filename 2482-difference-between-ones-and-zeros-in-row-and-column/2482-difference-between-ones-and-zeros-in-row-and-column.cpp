9da4ef63e7574ddd891806fdc8ae624545688ac5
Vivek_404_X <patelvpv369@gmail.com>
2026-08-08 16:10:39 +0530

    Time: 28 ms (77.55%), Space: 120.7 MB (95.11%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -8,14 +8,21 @@ public:
         int n=grid[0].size();
         vector<int>oR(m,0);
         vector<int>oC(n,0);
-        for(int i=0; i<m; i++){
-         for(int j=0; j<n; j++){
-            oR[i]+=grid[i][j];
-         }
-        }
+        // for(int i=0; i<m; i++){
+        //  for(int j=0; j<n; j++){
+        //     oR[i]+=grid[i][j];
+        //  }
+        // }
+        // for(int j=0; j<n; j++){
+        //  for(int i=0; i<m; i++){
+        //     oC[j]+=grid[i][j];
+        //  }
+        // }
+        //combine them to one and its also correct 
         for(int j=0; j<n; j++){
          for(int i=0; i<m; i++){
             oC[j]+=grid[i][j];
+            oR[i]+=grid[i][j];
          }
         }
         for(int i=0; i<m; i++){
