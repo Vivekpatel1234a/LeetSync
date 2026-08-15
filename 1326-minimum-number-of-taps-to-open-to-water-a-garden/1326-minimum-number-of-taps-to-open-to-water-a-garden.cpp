7e94612d02f2b78e84b896a822959f9d7d923555
Vivek_404_X <patelvpv369@gmail.com>
2026-08-15 22:39:18 +0530

    Time: 142 ms (10.71%), Space: 19.4 MB (42.44%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+       vector<int>garden(n);
+       vector<int>area(n+1,0);
+       for(int i=n; i>=0; i--){
+        int right=min(n,i+ranges[i]);
+        int left=max(0,i-ranges[i]);
+        area[i]=max(area[i],min(i+ranges[i],n));
+        area[left]=max(area[left],min(i+ranges[i],n));
+       }
+       int cnt=0;
+       int nxtidx=0;
+       int maxi=0;
+       for(auto ele:area)cout<<ele<<" ";
+       cout<<endl;
+       for(int i=0; i<=nxtidx; i++){
+        maxi=max(maxi,area[i]);
+        if(i==nxtidx){
+            //cout<<nxtidx<<" ";
+            nxtidx=maxi;
+            cnt++;
+        }
+       }
+       if(nxtidx<n)return -1;
+
+       return cnt-1;
+    }
+};
