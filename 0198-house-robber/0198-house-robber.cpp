4ca784ca25cb9f8c9ebbe53905548861e7ac5b63
Vivek_404_X <patelvpv369@gmail.com>
2026-08-21 15:43:10 +0530

    Time: 0 ms (100%), Space: 10 MB (90.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,4 @@
+/*//solve it from 0 to n bcz its n to 0
 class Solution {
 public:
     int dp[101];
@@ -7,6 +8,7 @@ public:
         if(dp[i]!=-1)return dp[i];
         int pick=nums[i]+solve(i-2,nums);
         int nopick=solve(i-1,nums);
+        cout<<pick<<" "<<nopick<<"   ";
         return dp[i]=max(pick,nopick);
     }
 
@@ -16,5 +18,23 @@ public:
         int i=n-1;
         int ans=solve(i,nums);
         return ans;
+    }
+};*/
+class Solution {
+public:
+    int dp[101];
+
+   int solve(int i, vector<int>&nums){
+    if(i>=nums.size())return 0;
+    if(dp[i]!=-1)return dp[i];
+    //if(i==nums.size()-1)return nums[i];
+    return dp[i]=max(solve(i+2,nums)+nums[i],solve(i+1,nums));
+   }
+
+    int rob(vector<int>& nums){
+        memset(dp,-1,sizeof(dp));
+        int ans=solve(0,nums);
+        return ans;
+
     }
 };
