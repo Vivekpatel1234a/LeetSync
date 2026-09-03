a7b761f6f5140aed5faeac5be7a9f2ee00743e02
Vivek_404_X <patelvpv369@gmail.com>
2026-09-04 02:06:06 +0530

    Time: 19 ms (26.11%), Space: 187.1 MB (5.41%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       int n=nums1.size();
+       vector<int>nums2(n);
+       vector<int>nextodd(n);
+       vector<int>nexteven(n);
+       //int nxt=INT_MAX;
+       int nxtodd=INT_MAX;
+       int nxteven=INT_MAX;
+       for(int i=n-1; i>=0; i--){
+        nextodd[i]=nxtodd;
+       if(nums1[i]%2!=0)nxtodd=min(nxtodd,nums1[i]);
+       }
+       for(int i=n-1; i>=0; i--){
+        nexteven[i]=nxteven;
+       if(nums1[i]%2==0)nxteven=min(nxteven,nums1[i]);
+       }
+      // for(auto ele:nextodd)cout<<ele<<" ";
+      // for(auto ele:nexteven)cout<<ele<<" ";
+       //check for even
+       int pre=INT_MAX;
+       int flage=1;
+       for(int i=0; i<n; i++){
+        if(nums1[i]%2!=0){
+            if(i==0 && nums1[i]<nextodd[i]){
+                flage=0;
+                break;
+            }
+            else if(i==n-1 && nums1[i]>pre){
+                flage=0;
+                break;
+            }
+            else if(nums1[i]<pre && nums1[i]<nextodd[i]){
+                flage=0;
+                break;
+            }
+         pre=min(pre,nums1[i]);
+        }
+       }
+       //check for odd
+       pre=INT_MAX;
+       int flago=1;
+       for(int i=0; i<n; i++){
+        if(nums1[i]%2==0){
+            if(i==0 && nums1[i]<nextodd[i]){
+                flago=0;
+                break;
+            }
+            else if(i==n-1 && pre>nums1[i]){
+                flago=0;
+                break;
+            }
+            else if(nums1[i]<pre && nums1[i]<nextodd[i]){
+                flago=0;
+                break;
+            }
+        }
+        else pre=min(pre,nums1[i]);
+       }
+       return flage||flago;
+    }
+};
