df94be8a735fe0329a19885a9af622dc6eeedef5
Vivek_404_X <patelvpv369@gmail.com>
2026-09-06 17:08:11 +0530

    Time: 0 ms (100%), Space: 103.1 MB (66.49%) - LeetHub

--- 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
@@ -2,30 +2,24 @@ class Solution {
 public:
     int countGoodRotations(vector<int>& nums) {
         int n=nums.size();
-        deque<int>q1;
-        deque<int>q2;
-        long long sum1=0;
-        for(int i=0; i<n/2; i++){
-            q1.push_back(nums[i]);
-            sum1+=nums[i];
-        }
-        long long sum2=0;
-        for(int i=n/2; i<n; i++){
-            q2.push_back(nums[i]);
-            sum2+=nums[i];
+        long sum1=0;
+        long sum2=0;
+        for(int i=0; i<n; i++){
+            if(i<n/2)sum1+=nums[i];
+            else sum2+=nums[i];
         }
         int cnt=0;
-        for(int i=0; i<n; i++){
-              if(sum1>sum2)cnt++;
-              q1.push_back(q2.front());
-              q2.pop_front();
-              q2.push_back(q1.front());
-              q1.pop_front();
-              sum1-=q2.back();
-              sum1+=q1.back();
-              sum2-=q1.back();
-              sum2+=q2.back();
+        int cnt2=0;
+        int k=n/2;
+        for(int i=0; i<k; i++){
+            if(sum1>sum2)cnt++;
+            else if(sum2>sum1)cnt2++;
+            sum1-=nums[i];
+            sum1+=nums[i+k];
+            sum2+=nums[i];
+            sum2-=nums[i+k];
+            
         }
-        return cnt;
+        return cnt+cnt2;
     }
 };
